e55efa057d509bad453cd818bb1e4c3cf47e6a13
bakali1 <bakalimohammed43@gamil.com>
2026-05-03 13:42:09 +0100

    trying to fix deploying

--- src/main/java/com/mstock/api/ApiApplication.java
+++ src/main/java/com/mstock/api/ApiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -13,15 +12,13 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 	@Bean
-public WebMvcConfigurer corsConfigurer() {
-  return new WebMvcConfigurer() {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-      registry.addMapping("/api/**")
-        .allowedOriginPatterns("*")
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+        .allowedOrigins(
+            "http://localhost:4200",
+            "http://localhost:8081"
+        )
         .allowedMethods("*")
         .allowedHeaders("*");
-    }
-  };
 }
 }
