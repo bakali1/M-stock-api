e9484af98d5ee7f4372521342d940448f107557d
bakali1 <bakalimohammed43@gamil.com>
2026-05-03 13:57:40 +0100

    Revert "changing origin accepted"
    This reverts commit 40262b9ee86e12e9d353e0078f8bd7d77b0e0264.

--- src/main/java/com/mstock/api/ApiApplication.java
+++ src/main/java/com/mstock/api/ApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -12,13 +13,15 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 	@Bean
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
-        .allowedOrigins(
-            "http://localhost:4200",
-            "http://localhost:8081"
-        )
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+        .allowedOrigins("http://localhost:4200","http://localhost:8081")
         .allowedMethods("*")
         .allowedHeaders("*");
+        }
+      };
 }
 }
